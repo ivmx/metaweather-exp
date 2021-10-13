2aa53b23971365fb8b227abfa21f508d198ea3d3
Ivan Hdez <ivan@comunidadfeliz.cl>
2021-10-13 02:09:25 -0600

    fea: add icon using weather state abbr

--- src/weather.model.js
+++ src/weather.model.js
@@ -10,16 +10,22 @@ const getWeather = function (basic, info) {
 //    - A property called `weather_icon_url` that is an URL to the MetaWeather “Weather States” icon applicable to the current weather in the response.
 //                        - example: `https://www.metaweather.com/static/img/weather/png/h.png`
     let the_temp = info.consolidated_weather[0].the_temp;
-    let temp_color_value = "#cc0000";
+    let state_abbr = info.consolidated_weather[0].weather_state_abbr;
+    let temp_color_value = "";
     if(the_temp < -1) {
         temp_color_value = "#6699ff";
     }
+    if(the_temp > 23) {
+        temp_color_value = "#cc0000";
+    }
+
+    weather_icon = `https://www.metaweather.com/static/img/weather/png/${state_abbr}.png`;
 
     return {
         current_temp: info.consolidated_weather[0].the_temp,
         city_name: basic.title,
-        the_temp_color_value: "#6699ff",
-        weather_icon_url: "https://www.metaweather.com/static/img/weather/png/h.png"
+        the_temp_color_value: temp_color_value,
+        weather_icon_url: weather_icon
     }
 };
 
